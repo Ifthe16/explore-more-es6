adb66a9112d6c6efcbf8da02593f519b121a9b71
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-01 23:44:57 +0600

    added dot file of javascript

---
+++ dot.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'kodom ali',
+    job: 'badam khai',
+    3: 'third',
+    'add-dress': 'kochu khet'
+}
+const prop = 'job';
+console.log(person.job);
+console.log(person['job']);
+console.log(person[prop]);
+// console.log(person.3);
+console.log(person['3']);
+// console.log(person.add-dress);
+// console.log(person.'add-dress');
+console.log(person["add-dress"]);
